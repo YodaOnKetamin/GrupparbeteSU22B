baf4780aa834128d4901c9ce7d65b73e074809f2
Blixt <blixtdraken@gmail.com>
2022-12-20 11:14:12 +0100

    Added Water Gun Script and Bullet SCript

---
+++ Assets/Scripts/WaterGun.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGun : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode fireKey = KeyCode.Mouse0;
+
+    private bool isFiring = false;
+    [SerializeField] 
+    private GameObject pelletPrefab;
+    
+    [SerializeField] 
+    private Transform shootPoint;
+
+    [SerializeField]
+    private Rigidbody2D playerRigidBody;
+
+    private Vector3 force;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Made a bool to check if the player is firing, and I made it in Fixedupdate so I have a ocnstant rate of fire
+        if (isFiring)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 shootDir = mousePos - (Vector2)transform.position;
+            force = shootDir.normalized * 500;
+            GameObject pellet = Instantiate(pelletPrefab, shootPoint.position, new Quaternion(0, 0, 90, 0));
+            pellet.GetComponent<WaterPellet>().SetForce(force);
+            playerRigidBody.AddForce((force *-1)/10);
+            
+        }
+        
+    }
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 shootDir = mousePos - (Vector2)transform.position;
+        float angle = Mathf.Atan2(shootDir.y, shootDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        
+        //Checks if the player is pressing the fire key
+        if (Input.GetKey(fireKey))
+        {
+            isFiring = true;
+        } else
+        {
+            isFiring = false;
+        }
+        
+    }
+}
+
---
+++ Assets/Scripts/WaterPellet.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPellet : MonoBehaviour
+{
+    private Vector3 force;
+
+    private Rigidbody2D rb;
+
+    public bool touchGround = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(force);
+        transform.Rotate(0, 0, 90);
+    }
+
+    private void Update()
+    {
+        if (!touchGround)
+        {
+            Vector2 v = rb.velocity.normalized;
+            float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.Rotate(0, 0, 90);
+        }
+       
+    }
+
+    public void SetForce(Vector3 force)
+    {
+        this.force = force;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        print("Collided");
+        if (col.gameObject.CompareTag("golv"))
+        {
+            print("Hit ground!");
+            rb.constraints = RigidbodyConstraints2D.None;
+            touchGround = true;
+        }else if(col.gameObject.CompareTag("WaterPellet"))
+        {
+            print("Hit another pellet!");
+            WaterPellet pellet = col.gameObject.GetComponent<WaterPellet>();
+            if (pellet.touchGround)
+            {
+                print("Hit another pellet that is on the ground!");
+                rb.constraints = RigidbodyConstraints2D.None;
+                touchGround = true;
+            }
+        }
+        
+    }
+}
